14b18344e2fc034e2be2c683a30b2a945a86cb1e
panzhiev <panzhiev.timur@gmail.com>
2017-03-17 01:09:52 +0200

    EditPerson On DetailActivity

--- app/src/main/java/com/tim/contentprovider/ui/activities/DetailsPersonsActivity.java
+++ app/src/main/java/com/tim/contentprovider/ui/activities/DetailsPersonsActivity.java
@@ -1,56 +1,79 @@
 package com.tim.contentprovider.ui.activities;
 
+import android.app.AlertDialog;
 import android.app.LoaderManager;
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.CursorLoader;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.tim.contentprovider.R;
 import com.tim.contentprovider.db.DBContentProvider;
 import com.tim.contentprovider.db.PersonContract;
+import com.tim.contentprovider.model.Person;
 import com.tim.contentprovider.utils.Utility;
 
 /**
  * Created by Tim on 21.01.2017.
  */
-public class DetailsPersonsActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>
-{
+public class DetailsPersonsActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
     TextView tvName, tvSurname, tvPhone, tvMail, tvSkype;
     ImageView ivProfile;
-    public  String TAG = "MY_LOG_DetailsPersonsActivity";
+    public ImageButton ibPhotoEdit;
+    public ImageButton ibEdit;
+    public String TAG = "MY_LOG_DetailsPersonsActivity";
+    final int REQUEST_CODE_PHOTO = 2;
+    public int idPerson;
+    public Person person;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        person = new Person();
         setContentView(R.layout.activity_person_details);
-        getLoaderManager().initLoader(0, null, this);
+        ibEdit = (ImageButton) findViewById(R.id.ib_edit);
+        ibEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openEditDialog(person);
+            }
+        });
+        ibPhotoEdit = (ImageButton) findViewById(R.id.ib_photo_edit);
+        ibPhotoEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, REQUEST_CODE_PHOTO);
+            }
+        });
 
-        tvName = (TextView)findViewById(R.id.text_view_details_name);
-        tvSurname = (TextView)findViewById(R.id.text_view_details_surname);
-        tvPhone = (TextView)findViewById(R.id.text_view_details_phone);
-        tvMail = (TextView)findViewById(R.id.text_view_details_mail);
-        tvSkype = (TextView)findViewById(R.id.text_view_details_skype);
+        Intent intent = this.getIntent();
+        Bundle bundle = intent.getExtras();
+        idPerson = bundle.getInt("IdPersonToDetailActivity");
+        getLoaderManager().initLoader(0, bundle, this);
+
+        tvName = (TextView) findViewById(R.id.text_view_details_name);
+        tvSurname = (TextView) findViewById(R.id.text_view_details_surname);
+        tvPhone = (TextView) findViewById(R.id.text_view_details_phone);
+        tvMail = (TextView) findViewById(R.id.text_view_details_mail);
+        tvSkype = (TextView) findViewById(R.id.text_view_details_skype);
         ivProfile = (ImageView) findViewById(R.id.image_view_main_profile);
-
-//        tvName.setText(Person.selectedPerson.getmName());
-//        tvSurname.setText(Person.selectedPerson.getmSurname());
-//        tvPhone.setText(Person.selectedPerson.getmPhone());
-//        tvMail.setText(Person.selectedPerson.getmMail());
-//        tvSkype.setText(Person.selectedPerson.getmSkype());
-
-//        String decodedPhoto = Person.selectedPerson.getmProfile();
-//        ivProfile.setImageBitmap(decodeBase64(decodedPhoto));
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        getLoaderManager().restartLoader(0, null, this);
     }
 
     @Override
@@ -58,10 +81,9 @@ public class DetailsPersonsActivity extends AppCompatActivity implements LoaderM
         String selection = null;
         String sortOrder = null;
 
-        if (bundle != null)
-        {
+        if (bundle != null) {
             int idPerson = bundle.getInt("IdPersonToDetailActivity");
-            selection = "IdPersonToDetailActivity = " + idPerson;
+            selection = "ID = " + idPerson;
             Log.d(TAG, "idPerson = " + idPerson);
         } else {
             Log.d(TAG, "bundle == null");
@@ -74,19 +96,103 @@ public class DetailsPersonsActivity extends AppCompatActivity implements LoaderM
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
-        while (cursor.moveToNext())
-        {
-            tvName.setText(cursor.getString(cursor.getColumnIndex(PersonContract.KEY_NAME)));
-            tvSurname.setText(cursor.getString(cursor.getColumnIndex(PersonContract.KEY_SURNAME)));
-            tvPhone.setText(cursor.getString(cursor.getColumnIndex(PersonContract.KEY_PHONE)));
-            tvMail.setText(cursor.getString(cursor.getColumnIndex(PersonContract.KEY_MAIL)));
-            tvSkype.setText(cursor.getString(cursor.getColumnIndex(PersonContract.KEY_SKYPE)));
+        while (cursor.moveToNext()) {
+            //достаем из БД данные, заполняем TextView в DetailsPersonsActivity;
+            tvName.setText("Name: " + cursor.getString(cursor.getColumnIndex(PersonContract.KEY_NAME)));
+            tvSurname.setText("Surname: " + cursor.getString(cursor.getColumnIndex(PersonContract.KEY_SURNAME)));
+            tvPhone.setText("Phone: " + cursor.getString(cursor.getColumnIndex(PersonContract.KEY_PHONE)));
+            tvMail.setText("Mail: " + cursor.getString(cursor.getColumnIndex(PersonContract.KEY_MAIL)));
+            tvSkype.setText("Skype: " + cursor.getString(cursor.getColumnIndex(PersonContract.KEY_SKYPE)));
             ivProfile.setImageBitmap(Utility.decodeBase64(cursor.getString(cursor.getColumnIndex(PersonContract.KEY_PROFILE))));
+
+            //достаем из БД данные, заполняем EditText в Dialog;
+            person.setmId(cursor.getInt(cursor.getColumnIndex(PersonContract.KEY_ID)));
+            person.setmName(cursor.getString(cursor.getColumnIndex(PersonContract.KEY_NAME)));
+            person.setmSurname(cursor.getString(cursor.getColumnIndex(PersonContract.KEY_SURNAME)));
+            person.setmPhone(cursor.getString(cursor.getColumnIndex(PersonContract.KEY_PHONE)));
+            person.setmMail(cursor.getString(cursor.getColumnIndex(PersonContract.KEY_MAIL)));
+            person.setmSkype(cursor.getString(cursor.getColumnIndex(PersonContract.KEY_SKYPE)));
         }
     }
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_CODE_PHOTO) {
+                if (intent == null) {
+                    Log.d(TAG, "Intent is null");
+                } else {
+                    Log.d(TAG, "Photo uri: " + intent.getData());
+                    Bundle bndl = intent.getExtras();
+                    if (bndl != null) {
+                        Object obj = intent.getExtras().get("data");
+                        if (obj instanceof Bitmap) {
+                            Bitmap bitmap = (Bitmap) obj;
+                            Log.d(TAG, "bitmap" + bitmap.getWidth() + " x " + bitmap.getHeight());
+
+                            String imageIncodedString = Utility.encodeToBase64(bitmap);
+                            ContentValues contentValues = new ContentValues();
+                            contentValues.put(PersonContract.KEY_PROFILE, imageIncodedString);
+                            getContentResolver().update(Uri.parse(DBContentProvider.PERSONS_CONTENT_URI + "/" + idPerson), contentValues, null, null);
+                        }
+                    }
+                }
+            }
+        } else if (resultCode == RESULT_CANCELED) {
+            Log.d(TAG, "Canceled");
+        }
+    }
+
+    private void openEditDialog(final Person person) {
+        LayoutInflater dialogInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        final View view = dialogInflater.inflate(R.layout.dialog_edit, null);
+
+        final TextView etDialogId = (TextView) view.findViewById(R.id.tv_text_dialog_id);
+        final EditText etDialogName = (EditText) view.findViewById(R.id.edit_text_dialog_name);
+        final EditText etDialogSurname = (EditText) view.findViewById(R.id.edit_text_dialog_surname);
+        final EditText etDialogPhone = (EditText) view.findViewById(R.id.edit_text_dialog_phone_number);
+        final EditText etDialogMail = (EditText) view.findViewById(R.id.edit_text_dialog_mail);
+        final EditText etDialogSkype = (EditText) view.findViewById(R.id.edit_text_dialog_skype);
+
+        etDialogId.setText(String.valueOf(person.getmId()));
+        etDialogName.setText(person.getmName());
+        etDialogSurname.setText(person.getmSurname());
+        etDialogPhone.setText(person.getmPhone());
+        etDialogMail.setText(person.getmMail());
+        etDialogSkype.setText(person.getmSkype());
+
+        final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setView(view);
+        alertDialogBuilder.setMessage("Edit Contact");
+
+        alertDialogBuilder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                ContentValues contentValues = new ContentValues();
+                contentValues.put(PersonContract.KEY_NAME, etDialogName.getText().toString());
+                contentValues.put(PersonContract.KEY_SURNAME, etDialogSurname.getText().toString());
+                contentValues.put(PersonContract.KEY_PHONE, etDialogPhone.getText().toString());
+                contentValues.put(PersonContract.KEY_MAIL, etDialogMail.getText().toString());
+                contentValues.put(PersonContract.KEY_SKYPE, etDialogSkype.getText().toString());
+                getContentResolver().update(Uri.parse(DBContentProvider.PERSONS_CONTENT_URI + "/" + person.getmId()), contentValues, null, null);
+            }
+        });
+        alertDialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                dialogInterface.cancel();
+            }
+        });
+        alertDialogBuilder.setCancelable(false);
+        alertDialogBuilder.create();
+        alertDialogBuilder.show();
     }
 }
--- app/src/main/java/com/tim/contentprovider/ui/adapters/FilterAdapter.java
+++ app/src/main/java/com/tim/contentprovider/ui/adapters/FilterAdapter.java
@@ -6,7 +6,6 @@ import android.app.AlertDialog;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.content.ContentValues;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.content.DialogInterface;
@@ -14,7 +13,6 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
@@ -22,25 +20,18 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 import android.widget.Filter;
 import android.widget.Filterable;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.tim.contentprovider.R;
 import com.tim.contentprovider.db.DBContentProvider;
-import com.tim.contentprovider.db.PersonContract;
 import com.tim.contentprovider.model.Person;
 import com.tim.contentprovider.ui.MainActivity;
 import com.tim.contentprovider.ui.activities.DetailsPersonsActivity;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static com.tim.contentprovider.utils.Utility.decodeBase64;
 
 //import com.tim.contentprovider.db.CRUDSharedPreferences;
 
@@ -87,12 +78,13 @@ public class FilterAdapter extends RecyclerView.Adapter<ViewHolderPerson> implem
         holder.tvName.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Person.selectedPerson = person;
-                int idPerson = personsList.get(position).getmId();
+                int idPerson = person.getmId();
                 Bundle bundle = new Bundle();
                 bundle.putInt("IdPersonToDetailActivity", idPerson);
-                Log.d(TAG, "idPerson = " + idPerson);
                 Intent intent = new Intent(mContext, DetailsPersonsActivity.class);
+                intent.putExtras(bundle);
+//                Person.selectedPerson = person;
+                Log.d(TAG, "idPerson = " + idPerson);
                 mContext.startActivity(intent);
             }
         });
@@ -105,12 +97,6 @@ public class FilterAdapter extends RecyclerView.Adapter<ViewHolderPerson> implem
                     return;
                 }
                 mContext.startActivity(intent);
-            }
-        });
-        holder.ibEdit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openEditDialog(person);
             }
         });
     }
@@ -129,88 +115,6 @@ public class FilterAdapter extends RecyclerView.Adapter<ViewHolderPerson> implem
     @Override
     public int getItemCount() {
         return personsList.size();
-    }
-
-    private void openEditDialog(final Person personItem) {
-        LayoutInflater dialogInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        final View view = dialogInflater.inflate(R.layout.dialog_edit, null);
-
-        final TextView etDialogId = (TextView) view.findViewById(R.id.tv_text_dialog_id);
-        final EditText etDialogName = (EditText) view.findViewById(R.id.edit_text_dialog_name);
-        final EditText etDialogSurname = (EditText) view.findViewById(R.id.edit_text_dialog_surname);
-        final EditText etDialogPhone = (EditText) view.findViewById(R.id.edit_text_dialog_phone_number);
-        final EditText etDialogMail = (EditText) view.findViewById(R.id.edit_text_dialog_mail);
-        final EditText etDialogSkype = (EditText) view.findViewById(R.id.edit_text_dialog_skype);
-        final ImageView ibPhotoEdit = (ImageButton) view.findViewById(R.id.ib_photo_edit);
-        final ImageView ivDialogPhoto = (ImageView) view.findViewById(R.id.image_view_main_profile_in_dialog);
-
-        ibPhotoEdit.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                getActivityFromContext(mContext).startActivityForResult(intent, 2);
-
-//                if (newPhoto == null)
-//                {
-//                    Toast.makeText(mContext, "newPhoto = null", Toast.LENGTH_SHORT).show();
-//                }else
-//                {
-//                    ivDialogPhoto.setImageBitmap(decodeBase64(newPhoto));
-//                    personItem.setmProfile(newPhoto);
-//                    notifyDataSetChanged();
-//                }
-            }
-        });
-
-        String decodedPhoto = personItem.getmProfile();
-        ivDialogPhoto.setImageBitmap(decodeBase64(decodedPhoto));
-
-        etDialogId.setText(String.valueOf(personItem.getmId()));
-        etDialogName.setText(personItem.getmName());
-        etDialogSurname.setText(personItem.getmSurname());
-        etDialogPhone.setText(personItem.getmPhone());
-        etDialogMail.setText(personItem.getmMail());
-        etDialogSkype.setText(personItem.getmSkype());
-
-        final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(mContext);
-        alertDialogBuilder.setView(view);
-        alertDialogBuilder.setMessage("Edit Contact");
-
-        alertDialogBuilder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                personItem.setmName(etDialogName.getText().toString());
-                personItem.setmSurname(etDialogSurname.getText().toString());
-                personItem.setmPhone(etDialogPhone.getText().toString());
-                personItem.setmMail(etDialogMail.getText().toString());
-                personItem.setmSkype(etDialogSkype.getText().toString());
-
-                ContentValues contentValues = new ContentValues();
-                contentValues.put(PersonContract.KEY_NAME, etDialogName.getText().toString());
-                contentValues.put(PersonContract.KEY_SURNAME, etDialogSurname.getText().toString());
-                contentValues.put(PersonContract.KEY_PHONE, etDialogPhone.getText().toString());
-                contentValues.put(PersonContract.KEY_MAIL, etDialogMail.getText().toString());
-                contentValues.put(PersonContract.KEY_SKYPE, etDialogSkype.getText().toString());
-                mContext.getContentResolver().update(Uri.parse(DBContentProvider.PERSONS_CONTENT_URI + "/" + personItem.getmId()), contentValues, null, null);
-                notifyDataSetChanged();
-            }
-        });
-        alertDialogBuilder.setNeutralButton("Delete", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                openDeleteDialog(personItem);
-                sendNotification(personItem);
-            }
-        });
-        alertDialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int which) {
-                dialogInterface.cancel();
-            }
-        });
-        alertDialogBuilder.setCancelable(false);
-        alertDialogBuilder.create();
-        alertDialogBuilder.show();
     }
 
     private void openDeleteDialog(final Person personItem) {
